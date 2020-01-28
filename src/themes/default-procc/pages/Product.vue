cdaf331425210b7c53c390f0f2c69eb037f7aa2d
Dan <d.irinkovn1@gmail.com>
2020-01-29 00:58:00 +0200

    Changed the Shipping logo (Needs polishing not tested)

--- src/themes/default-procc/pages/Product.vue
+++ src/themes/default-procc/pages/Product.vue
@@ -178,7 +178,7 @@
                           width="50px"
                           height="50px"
                           :src="getCurrentProduct.brand_logo"
-                          alt="Vuestore logo"
+                          alt="Brand logo"
                         >
                       </div>
                       <div class="align-center">
