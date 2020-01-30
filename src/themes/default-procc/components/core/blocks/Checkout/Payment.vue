af13fee59bca4a2422cd9977d91c76b739088c9e
Dan <d.irinkovn1@gmail.com>
2020-01-30 02:37:34 +0200

    i18n text changes shipping icon resize

--- src/themes/default-procc/components/core/blocks/Checkout/Payment.vue
+++ src/themes/default-procc/components/core/blocks/Checkout/Payment.vue
@@ -37,7 +37,7 @@
             v-model="sendToShippingAddress"
             v-if="!isVirtualCart"
           >
-            {{ $t('Copy address data from shipping') }}
+            {{ $t('The billing address is the same') }}
           </base-checkbox>
         </div>
         <!--modify condition for show address at copy shipping address unchecked by shabbir -->
