b28dffb074206730aef1d72e4e5ed9f8cca81abe
shabbir-bharmal <shabbir.bharmal@arsenaltech.com>
2020-04-14 20:00:51 +0530

    - Created country list file - Display countries list in modal

---
+++ src/themes/default-procc/helpers/countries.js
@@ -0,0 +1,60 @@
+const countries = [
+  {
+    name: 'United States',
+    language: 'en-US',
+    lang_name: 'English'
+  },
+  {
+    name: 'Germany',
+    language: 'de-DE',
+    lang_name: 'German'
+  },
+  {
+    name: 'France',
+    language: 'fr-FR',
+    lang_name: 'French'
+  },
+  {
+    name: 'Spain',
+    language: 'es-ES',
+    lang_name: 'Spanish'
+  },
+  {
+    name: 'Netherlands',
+    language: 'nl-NL',
+    lang_name: 'Dutch'
+  },
+  {
+    name: 'Japan',
+    language: 'ja-JP',
+    lang_name: 'Japanese'
+  },
+  {
+    name: 'Russia',
+    language: 'ru-RU',
+    lang_name: 'Russian'
+  },
+  {
+    name: 'Italy',
+    language: 'it-IT',
+    lang_name: 'Italian'
+  },
+  {
+    name: 'Brazil',
+    language: 'pt-BR',
+    lang_name: 'Portuguese'
+  },
+  {
+    name: 'Poland',
+    language: 'pl-PL',
+    lang_name: 'Polish'
+  },
+  {
+    name: 'Czech',
+    language: 'cs-CZ',
+    lang_name: 'Czech'
+  }]
+
+export {
+  countries
+}
